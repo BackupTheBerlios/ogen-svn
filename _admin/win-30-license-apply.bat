15331502f3933fd8fbcef0e38f1c0f8ad634f39e
fmonteiro <fmonteiro@a9f11ab3-d4fa-0310-9c37-a022b8bb196d>
2007-03-14 00:43:51 +0000

    changing license, moving from GPL to MIT, step 1

--- _admin/win-30-license-apply.bat
+++ _admin/win-30-license-apply.bat
@@ -39,17 +39,22 @@ SET license_lgpl=f
 FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'GNU_LGPL' SET license_lgpl=t
 SET license_fdl=f
 FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'GNU_FDL' SET license_fdl=t
+SET license_mit=f
+FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'MIT' SET license_mit=t
 
 ::--- <LICENSE.txt>
 ECHO.>licenses\LICENSE.no-svn.txt
 ECHO OGen is licensed under various different licenses.>>licenses\LICENSE.no-svn.txt
 ECHO.>>licenses\LICENSE.no-svn.txt
+IF '%license_mit%' == 't' ECHO 	MIT: details available in the file LICENSE.MIT.txt>>licenses\LICENSE.no-svn.txt
 IF '%license_gpl%' == 't' ECHO 	GNU GPL: details avaliable in the file LICENSE.GPL.txt>>licenses\LICENSE.no-svn.txt
 IF '%license_lgpl%' == 't' ECHO 	GNU LGPL: details available in the file LICENSE.LGPL.txt>>licenses\LICENSE.no-svn.txt
 IF '%license_fdl%' == 't' ECHO 	GNU FDL: details available in the file LICENSE.FDL.txt>>licenses\LICENSE.no-svn.txt
 ECHO.>>licenses\LICENSE.no-svn.txt
-ECHO 	For your convenience copies of the GNU GPL, GNU LGPL and GNU FDL are located in the file COPYING, COPYING.LIB and COPYING.DOC>>licenses\LICENSE.no-svn.txt
+::ECHO 	For your convenience copies of the MIT, GNU GPL, GNU LGPL and GNU FDL are located in the file COPYING.MIT COPYING, COPYING.LIB and COPYING.DOC
+ECHO 	For your convenience copies of the MIT and GNU FDL are located in the file COPYING.MIT and COPYING.DOC>>licenses\LICENSE.no-svn.txt
 ECHO.>>licenses\LICENSE.no-svn.txt
+FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'MIT' ECHO 	* %%c: MIT>>licenses\LICENSE.no-svn.txt
 FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'GNU_GPL' ECHO 	* %%c: GNU GPL>>licenses\LICENSE.no-svn.txt
 FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'GNU_LGPL' ECHO 	* %%c: GNU LGPL>>licenses\LICENSE.no-svn.txt
 FOR /F "tokens=1,2,3,4,5,6,7 delims=, " %%a IN (OGen-projects.txt) DO IF '%%g' == 'GNU_FDL' ECHO 	* %%c: GNU FDL>>licenses\LICENSE.no-svn.txt
@@ -61,6 +66,8 @@ SET license_lgpl=
 SET license_fdl=
 
 
+FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\COPYING.MIT DEL /q /f ..\%%a\COPYING.MIT
+FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\LICENSE.MIT.txt DEL /q /f ..\%%a\LICENSE.MIT.txt
 FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\COPYING DEL /q /f ..\%%a\COPYING
 FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\LICENSE.GPL.txt DEL /q /f ..\%%a\LICENSE.GPL.txt
 FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\COPYING.LIB DEL /q /f ..\%%a\COPYING.LIB
@@ -87,6 +94,18 @@ GOTO eof
 
 
 :install_step1
+	IF NOT '%7' == 'MIT' GOTO skip_mit
+		COPY licenses\COPYING.MIT ..\%1\%2\COPYING.MIT
+		COPY licenses\LICENSE.MIT.txt ..\%1\%2\LICENSE.MIT.txt
+		FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO COPY /y licenses\COPYING.MIT ..\%%a\COPYING.MIT
+		FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO COPY /y licenses\LICENSE.MIT.txt ..\%%a\LICENSE.MIT.txt
+	GOTO install_step2
+	:skip_mit
+		IF EXIST ..\%1\%2\COPYING.MIT				DEL /q /f ..\%1\%2\COPYING.MIT
+		IF EXIST ..\%1\%2\LICENSE.MIT.txt		DEL /q /f ..\%1\%2\LICENSE.MIT.txt
+		::FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\COPYING.MIT DEL /q /f ..\%%a\COPYING.MIT
+		::FOR /F "tokens=1,2 delims=, " %%a IN (OGen-solutions.txt) DO IF EXIST ..\%%a\LICENSE.MIT.txt DEL /q /f ..\%%a\LICENSE.MIT.txt
+
 	IF NOT '%7' == 'GNU_GPL' GOTO skip_gpl
 		COPY licenses\COPYING ..\%1\%2\COPYING
 		COPY licenses\LICENSE.GPL.txt ..\%1\%2\LICENSE.GPL.txt
