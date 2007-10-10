04d32bc169df8ee64400343bc5cfb0e35618d028
fmonteiro <fmonteiro@a9f11ab3-d4fa-0310-9c37-a022b8bb196d>
2007-10-10 11:02:46 +0000

    backup

--- OGen-XSD/XSD_metadata/xs__collection.cs
+++ OGen-XSD/XSD_metadata/xs__collection.cs
@@ -19,11 +19,16 @@ using System.Collections;
 
 namespace OGen.XSD.lib.metadata {
 	public class utils {
-		private utils() {
-		}
+		private utils() {}
 
 		public static void ReflectThrough(object someClass_in) {
-			PropertyInfo[] _properties = someClass_in.GetType().GetProperties(
+			PropertyInfo[] _properties;
+			System.Xml.Serialization.XmlElementAttribute _elementAttribute;
+			System.Xml.Serialization.XmlAttributeAttribute _attribute;
+			object _value;
+			Array _array;
+
+			_properties = someClass_in.GetType().GetProperties(
 				BindingFlags.Public | 
 				BindingFlags.Instance
 			);
@@ -32,67 +37,73 @@ namespace OGen.XSD.lib.metadata {
 					_properties[_prop], 
 					typeof(System.Xml.Serialization.XmlAttributeAttribute)
 				)) {
+					_value = _properties[_prop].GetValue(someClass_in, null);
+
+					if (_value == null) return;
+
+					//Console.Write(
+					//	"XmlAttribute::{0}", 
+					//	_properties[_prop].Name
+					//);
+
+					_attribute 
+						= (System.Xml.Serialization.XmlAttributeAttribute)Attribute.GetCustomAttributes(
+							_properties[_prop], 
+							typeof(System.Xml.Serialization.XmlAttributeAttribute), 
+							true
+						)[0];
 					Console.Write(
-						"XmlAttribute::{0}", 
-						_properties[_prop].Name
+						"{0}=\"{1}\" ",
+						_attribute.AttributeName, 
+						_value.ToString()
 					);
-
-					Attribute[] _attributes = Attribute.GetCustomAttributes(
-						_properties[_prop], 
-						typeof(System.Xml.Serialization.XmlAttributeAttribute), 
-						true
-					);
-					for (int _att = 0; _att < _attributes.Length; _att++) {
-						//if (_attributes[_att].GetType() == typeof(System.Xml.Serialization.XmlAttributeAttribute)) {
-							System.Xml.Serialization.XmlAttributeAttribute _attribute 
-								= (System.Xml.Serialization.XmlAttributeAttribute)_attributes[_att];
-							Console.WriteLine(
-								" - AttributeName:{0};",//  DataType:{1};  Type:{2};  ",
-								_attribute.AttributeName,
-								_attribute.DataType,
-								_attribute.Type
-							);
-						//}
-					}
-
 				} else if (Attribute.IsDefined(
 					_properties[_prop], 
 					typeof(System.Xml.Serialization.XmlElementAttribute)
 				)) {
-					Console.Write(
-						"XmlElementAttribute::{0}", 
-						_properties[_prop].Name
-					);
-
-					Attribute[] _attributes = Attribute.GetCustomAttributes(
-						_properties[_prop], 
-						typeof(System.Xml.Serialization.XmlElementAttribute), 
-						true
-					);
-					for (int _att = 0; _att < _attributes.Length; _att++) {
-						//if (_attributes[_att].GetType() == typeof(System.Xml.Serialization.XmlElementAttribute)) {
-							System.Xml.Serialization.XmlElementAttribute _attribute 
-								= (System.Xml.Serialization.XmlElementAttribute)_attributes[_att];
-							Console.WriteLine(
-								" - ElementName:{0};",//  DataType:{1};  Type:{2};  ",
-								_attribute.ElementName,
-								_attribute.DataType,
-								_attribute.Type
-							);
-						//}
-					}
-
-					object _value = _properties[_prop].GetValue(someClass_in, null);
+					_value = _properties[_prop].GetValue(someClass_in, null);
 
 					if (_value == null) return;
 
+					//Console.Write(
+					//	"XmlElementAttribute::{0}", 
+					//	_properties[_prop].Name
+					//);
+
+					_elementAttribute 
+						= (System.Xml.Serialization.XmlElementAttribute)Attribute.GetCustomAttributes(
+							_properties[_prop], 
+							typeof(System.Xml.Serialization.XmlElementAttribute), 
+							true
+						)[0];
+
 					if (_value.GetType().IsArray) {
-						Array _array = (Array)_value;
+						_array = (Array)_value;
 						for (int i = 0; i < _array.Length; i++) {
+							Console.Write(
+								"\n<xs:{0} ",
+								_elementAttribute.ElementName
+							);
+
 							ReflectThrough(_array.GetValue(i));
+
+							Console.Write(
+								"\n</xs:{0}>",
+								_elementAttribute.ElementName
+							);
 						}
 					} else {
+						Console.Write(
+							"\n<xs:{0} ",
+							_elementAttribute.ElementName
+						);
+
 						ReflectThrough(_value);
+
+						Console.Write(
+							"\n</xs:{0}>",
+							_elementAttribute.ElementName
+						);
 					}
 				}
 			}
